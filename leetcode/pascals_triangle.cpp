2e6fbb66934d400981047a8c0c975692396d913d
clumsy <clumsyroot.outlook.com>
2023-04-05 20:46:13 +0800

    regular commit

--- leetcode/pascals_triangle.cpp
+++ leetcode/pascals_triangle.cpp
@@ -7,18 +7,46 @@ using std::vector;
 class Solution
 {
 public:
+    // 杨辉三角
     vector<vector<int>> generate(int numRows)
     {
         vector<vector<int>> ans(numRows);
         for (int i = 0; i < numRows; i++)
         {
-            ans[i].resize(i);
+            ans[i].resize(i + 1);
+            ans[i][0] = ans[i][i] = 1;
+            for (int j = 1; j < i; j++)
+            {
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
         }
+        return ans;
+    }
+    // 杨辉三角 II
+    vector<int> getRow(int rowIndex)
+    {
+        // 滚动数组
+        vector<int> pre, cur(1);
+        int idx = 0;
+        int front;
+        while (idx <= rowIndex)
+        {
+            cur.resize(idx + 1);
+            cur[0] = cur[idx] = 1;
+            for (int i = 1; i < idx; i++)
+            {
+                cur[i] = pre[i - 1] + pre[i];
+            }
+            pre = cur;
+            idx++;
+        }
+        return cur;
     }
 };
 
 int main()
 {
-
+    Solution solution;
+    solution.getRow(3);
     return 0;
 }
