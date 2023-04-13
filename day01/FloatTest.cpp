329cf3ddb855b3a66f8bf336fe5014b9ee5e6337
clumsy <clumsyroot.outlook.com>
2023-04-13 20:34:12 +0800

    regular commit

---
+++ day01/FloatTest.cpp
@@ -0,0 +1,14 @@
+#include <cfloat>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    float max_float = FLT_MAX;
+    cout << max_float << endl;
+
+    cout << "Double Max is " << DBL_MAX << endl;
+    cout << "Double Min is " << DBL_MIN << endl;
+
+    return 0;
+}
