0ac91a1cb49ee87cf4e95ed762e37e3150475a2b
clumsy <clumsyroot.outlook.com>
2023-04-18 21:17:08 +0800

    regular commit

--- day01/pointer.cpp
+++ day01/pointer.cpp
@@ -5,6 +5,7 @@ int main()
     using namespace std;
     int updates = 6;
     int *p_updates; // * 运算符, 将其应用于指针, 可以得到该地址处存储的值
+    double *d_updates;
     p_updates = &updates;
 
     cout << "Value: updates = " << updates;
@@ -14,6 +15,9 @@ int main()
 
     *p_updates = *p_updates + 1;
     cout << "Now updates = " << updates << endl;
+
+    cout << "Size of Int * is " << sizeof(p_updates) << " Bytes." << endl;
+    cout << "Size of Double * is " << sizeof(d_updates) << " Bytes." << endl;
 
     return 0;
 }
