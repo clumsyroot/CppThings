759f2f34fa25ae5786fc75b8be15a2047a445c22
clumsy <clumsyroot@outlook.com>
2023-05-05 20:58:40 +0800

    regular commit

---
+++ day02/arg_process.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+    int opt;
+    const char *str = "p:l:m:o:s:t:c:a:";
+    for (int i = 0; i < argc; i++)
+    {
+        cout << argv[i] << endl;
+    }
+
+    while ((opt = getopt(argc, argv, str)) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            printf("opt is p, optarg is: %s\n", optarg);
+            break;
+        case 'l':
+            printf("opt is l, optarg is: %s\n", optarg);
+            break;
+        case 'm':
+            printf("opt is m, optarg is: %s\n", optarg);
+            break;
+        case 'o':
+            printf("opt is o, optarg is: %s\n", optarg);
+            break;
+        case 's':
+            printf("opt is s, optarg is: %s\n", optarg);
+            break;
+        case 't':
+            printf("opt is t, optarg is: %s\n", optarg);
+            break;
+        case 'c':
+            printf("opt is c, optarg is: %s\n", optarg);
+            break;
+        case 'a':
+            printf("opt is a, optarg is: %s\n", optarg);
+            break;
+        default:
+            cout << "Invalid option: " << opt << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
