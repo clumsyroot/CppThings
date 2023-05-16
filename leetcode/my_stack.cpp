94ae41e78aa944350dc60361a5e84dee4a9d0864
clumsy <clumsyroot@outlook.com>
2023-05-16 16:57:58 +0800

    regular commit

---
+++ leetcode/my_stack.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+class MyStack
+{
+private:
+    std::vector<int> m_index;
+
+public:
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        index.emplace_back(x);
+    }
+
+    int pop()
+    {
+        int _x = index.back();
+        index.erase(index.end() - 1);
+        return _x;
+    }
+
+    int top()
+    {
+        return index.back();
+    }
+
+    bool empty()
+    {
+        if (index.size() == 0) return true;
+        return false;
+    }
+};
+
+int main()
+{
+    using namespace std;
+    MyStack m1;
+    m1.push(1);
+    m1.push(2);
+    cout << m1.top() << endl;
+    cout << m1.empty() << endl;
+
+    return 0;
+}
+
+Time Time::sum()(const Time &t) const
+{
+    Time sum;
+    sum.minutes = minutes + t.minutes;
+    sum.hours = hours + t.hours + sum.minutes / 60;
+    sum.minutes % %= 60;
+    return sum;
+}
