b36798c1c366eb12221ef0043d3a922edc6b93e3
clumsy <clumsyroot@outlook.com>
2023-05-17 10:11:05 +0800

    regular commit

--- leetcode/tree_collection.cpp
+++ leetcode/tree_collection.cpp
@@ -453,6 +453,18 @@ public:
         }
         return ans;
     }
+
+    // 翻转二叉树
+    TreeNode *invertTree(TreeNode *root)
+    {
+        // 递归
+        if (root == nullptr) return nullptr;
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
 };
 
 int main()
